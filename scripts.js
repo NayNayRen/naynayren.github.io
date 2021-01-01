b2df9d07c9a144870a6414ebdb897e5448c7c36a
Nate M <jeditendencies35@gmail.com>
2021-01-01 13:35:36 -0500

    adjustments and movements to section heading

--- scripts.js
+++ scripts.js
@@ -4,9 +4,9 @@ const nav = document.querySelector('.nav-container');
 const scrollPoint = document.querySelector('#scroll-point');
 
 function fixedNav() {
-  if (document.documentElement.scrollTop > 125) {// scroll point in pixels to start the fixed position
+  if (document.documentElement.scrollTop > 75) {// scroll point in pixels to start the fixed position
     nav.classList.add('fixed');
-    scrollPoint.style.paddingTop = '115px';// padding of the scroll point when fixed
+    scrollPoint.style.paddingTop = '185px';// padding of the scroll point when fixed
   } else {
     nav.classList.remove('fixed');
     scrollPoint.style.paddingTop = '35px';// padding of the scroll point when not fixed
