4d4fd763f9ca20cfb70aa098001c4a7c3a88a240
Nate M <jeditendencies35@gmail.com>
2021-01-03 00:39:33 -0500

    more alterations to tiny screen widths

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ function fixedNav() {
     nav.classList.remove('fixed');
     scrollPoint.style.paddingTop = '35px';// padding of the scroll point when not fixed
   }
-  if(document.documentElement.scrollTop > 70 && window.innerWidth <= 700){
+  if(document.documentElement.scrollTop > 70 && window.innerWidth <= 700){// changes scroll point and padding for 700px width
     nav.classList.add('fixed');
     scrollPoint.style.paddingTop = '110px';
   }
