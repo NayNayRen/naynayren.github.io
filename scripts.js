05e147619279fc42695035f7235addc56ba48849
Nate M <jeditendencies35@gmail.com>
2021-01-03 00:29:28 -0500

    moved and colored sectiion titles

--- scripts.js
+++ scripts.js
@@ -7,9 +7,13 @@ function fixedNav() {
   if (document.documentElement.scrollTop > 75) {// scroll point in pixels to start the fixed position
     nav.classList.add('fixed');
     scrollPoint.style.paddingTop = '185px';// padding of the scroll point when fixed
-  } else {
+  }else {
     nav.classList.remove('fixed');
     scrollPoint.style.paddingTop = '35px';// padding of the scroll point when not fixed
+  }
+  if(document.documentElement.scrollTop > 70 && window.innerWidth <= 700){
+    nav.classList.add('fixed');
+    scrollPoint.style.paddingTop = '110px';
   }
 }
 
