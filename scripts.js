5e594cd2e947e4601d69e7189d50718972fce717
Nate M <jeditendencies35@gmail.com>
2021-01-25 15:08:30 -0500

    added box shadow to nav box when scrolled, changed slide scale and slide button color on hover

--- scripts.js
+++ scripts.js
@@ -6,13 +6,17 @@ const scrollPoint = document.querySelector('#scroll-point');
 function fixedNav() {
   if (document.documentElement.scrollTop > 75) {// scroll point in pixels to start the fixed position
     nav.classList.add('fixed');
+    nav.style.transition = 'box-shadow 200ms ease-in';
+    nav.style.boxShadow = '0 60px 50px 50px rgba(0,0,0,0.75)';
     scrollPoint.style.paddingTop = '185px';// padding of the scroll point when fixed
   }else {
     nav.classList.remove('fixed');
+    nav.style.boxShadow = 'none';
     scrollPoint.style.paddingTop = '35px';// padding of the scroll point when not fixed
   }
   if(document.documentElement.scrollTop > 70 && window.innerWidth <= 700){// changes scroll point and padding for 700px width
     nav.classList.add('fixed');
+    nav.style.boxShadow = 'none';
     scrollPoint.style.paddingTop = '110px';
   }
 }
