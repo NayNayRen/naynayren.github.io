6e64d9318164d0b859e4df9681f6846e8f415c16
Nate M <jeditendencies35@gmail.com>
2021-04-16 11:19:31 -0400

    font, line height, spacing, border color changed

--- scripts.js
+++ scripts.js
@@ -8,11 +8,11 @@ function fixedNav() {
     nav.classList.add('fixed');
     nav.style.transition = 'box-shadow 200ms ease-in';
     nav.style.boxShadow = '0 60px 50px 50px rgba(0,0,0,0.75)';
-    scrollPoint.style.paddingTop = '185px';// padding of the scroll point when fixed
+    scrollPoint.style.paddingTop = '130px';// padding of the scroll point when fixed
   }else {
     nav.classList.remove('fixed');
     nav.style.boxShadow = 'none';
-    scrollPoint.style.paddingTop = '35px';// padding of the scroll point when not fixed
+    scrollPoint.style.paddingTop = '45px';// padding of the scroll point when not fixed
   }
   if(document.documentElement.scrollTop > 70 && window.innerWidth <= 700){// changes scroll point and padding for 700px width
     nav.classList.add('fixed');
