636f2753f7eab72444ac6ce416acdf143c51544e
Nate M <jeditendencies35@gmail.com>
2021-11-09 15:00:02 -0500

    added a fade move in from the right transition on the container squares

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ const burgerMenu = document.querySelector('.burger-menu');
 const nav = document.querySelector('.nav-container');
 const scrollPoint = document.getElementById('scroll-point');
 const containers = document.querySelectorAll('.container');
+const education = document.querySelector('.education');
+const containerSquares = document.querySelectorAll('.container-square');
 
 // fixed navigation settings for all screens
 function fixedNav() {
@@ -9,27 +11,27 @@ function fixedNav() {
   if (document.documentElement.scrollTop > 120 && window.innerWidth > 1300) {
     nav.style.position = 'fixed';
     nav.style.top = '0';
-    document.querySelector('.up-arrow').style.left = '0';
     scrollPoint.style.paddingTop = '80px';
+    document.querySelector('.up-arrow').style.left = '0';
   } else if (document.documentElement.scrollTop > 105 && window.innerWidth < 1300 && window.innerWidth > 700) {
     nav.style.position = 'fixed';
     nav.style.top = '0';
-    document.querySelector('.up-arrow').style.left = '0';
     scrollPoint.style.paddingTop = '75px';
+    document.querySelector('.up-arrow').style.left = '0';
   } else if (document.documentElement.scrollTop > 85 && window.innerWidth < 700 && window.innerWidth > 400) {
     nav.style.position = 'fixed';
     nav.style.top = '0';
-    document.querySelector('.up-arrow').style.left = '0';
     scrollPoint.style.paddingTop = '35px';
+    document.querySelector('.up-arrow').style.left = '0';
   } else if (document.documentElement.scrollTop > 60 && window.innerWidth < 400) {
     nav.style.position = 'fixed';
     nav.style.top = '0';
-    document.querySelector('.up-arrow').style.left = '0';
     scrollPoint.style.paddingTop = '25px';
+    document.querySelector('.up-arrow').style.left = '0';
   } else {
     nav.style.position = 'relative';
-    document.querySelector('.up-arrow').style.left = '-60px';
     scrollPoint.style.paddingTop = '0px';
+    document.querySelector('.up-arrow').style.left = '-60px';
   }
 }
 
@@ -51,11 +53,18 @@ function readMore(project) {
 }
 
 // fades each container for the scroll effect
-function fadeIn(container) {
-  container.style.opacity = '1';
-  container.style.transition = 'opacity 750ms ease';
+function fadeIn(element) {
+  element.style.opacity = '1';
+  element.style.transition = 'opacity 750ms ease-in';
+}
+// fades each square for the scroll effect
+function moveSquares(element) {
+  element.style.right = '-100px';
+  element.style.transition = 'right 650ms ease-out';
 }
 
+// on page scroll sticky nav container takes hold
+window.addEventListener('scroll', fixedNav);
 // toggles the links menu left and right when clicked
 burgerMenu.addEventListener('click', () => {
   document.querySelector('.nav-links-container').classList.toggle('move-nav-links-on');
@@ -64,7 +73,6 @@ burgerMenu.addEventListener('click', () => {
   document.querySelector('#burger-bars-3').classList.toggle('burger-bars-rotate-counter-clockwise');
   document.querySelector('#burger-bars-4').classList.toggle('burger-bars-remove');
 });
-
 // on page scroll containers are faded in and out when reaching the distance from the top and according to window width
 window.addEventListener('scroll', () => {
   containers.forEach(container => {
@@ -81,6 +89,35 @@ window.addEventListener('scroll', () => {
       container.style.opacity = '0';
     }
   });
+  containerSquares.forEach(square => {
+    const squareDistanceFromTopOfPage = square.getBoundingClientRect().top;
+    if (squareDistanceFromTopOfPage < 425 && window.innerWidth > 1000) {
+      moveSquares(square);
+    } else if (squareDistanceFromTopOfPage < 400 && window.innerWidth < 1000 && window.innerWidth > 700) {
+      moveSquares(square);
+    } else {
+      square.style.right = '-1000px';
+    }
+  });
 });
-// on page scroll sticky nav container takes hold
-window.addEventListener('scroll', fixedNav);
+// on page load, checks to see where education container is according to page top, and adds opacity
+window.addEventListener('load', () => {
+  const distanceFromEducationToTopOfPage = education.getBoundingClientRect().top;
+  if (distanceFromEducationToTopOfPage < 650 && window.innerWidth > 1000) {
+    education.style.opacity = '1';
+  } else if (distanceFromEducationToTopOfPage < 575 && window.innerWidth < 1000 && window.innerWidth > 700) {
+    education.style.opacity = '1';
+  } else {
+    education.style.opacity = '0';
+  }
+  containerSquares.forEach(square => {
+    const squareDistanceFromTopOfPage = square.getBoundingClientRect().top;
+    if (squareDistanceFromTopOfPage < 425 && window.innerWidth > 1000) {
+      moveSquares(square);
+    } else if (squareDistanceFromTopOfPage < 400 && window.innerWidth < 1000 && window.innerWidth > 700) {
+      moveSquares(square);
+    } else {
+      square.style.right = '-1000px';
+    }
+  });
+});
