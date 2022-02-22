6a6c232970d874f3a70fc03e1137d0efbd0dc4e8
Nate M <jeditendencies35@gmail.com>
2022-02-22 12:34:54 -0500

    scrolling is a little sketchy now

--- scripts.js
+++ scripts.js
@@ -31,7 +31,7 @@ function fixedNav() {
   } else if (document.documentElement.scrollTop > 65 && window.innerWidth < 400) {
     nav.style.position = 'fixed';
     nav.style.top = '0';
-    scrollPoint.style.paddingTop = '5px';
+    scrollPoint.style.paddingTop = '0px';
     headerLogo.style.top = '7px';
     upArrow.style.right = '5px';
   } else {
