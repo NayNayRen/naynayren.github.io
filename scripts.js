dcd4abb291911b6ce8318e3360695ed564f6db02
Nate M <jeditendencies35@gmail.com>
2022-02-23 15:58:18 -0500

    moved nav container inside header, see if it fixes the overflow deal on mobile

--- scripts.js
+++ scripts.js
@@ -52,16 +52,16 @@ function fixedNav() {
     scrollPoint.style.paddingTop = '75px';
     headerLogo.style.top = '15px';
     upArrow.style.right = '5px';
-  }else if (document.documentElement.scrollTop > 85 && window.innerWidth < 700 && window.innerWidth > 400) {
+  }else if (document.documentElement.scrollTop > 90 && window.innerWidth < 700 && window.innerWidth > 400) {
     nav.style.position = 'fixed';
     nav.style.top = '0';
-    scrollPoint.style.paddingTop = '35px';
+    scrollPoint.style.paddingTop = '0px';
     headerLogo.style.top = '7px';
     upArrow.style.right = '5px';
   } else if (document.documentElement.scrollTop > 65 && window.innerWidth < 400) {
     nav.style.position = 'fixed';
     nav.style.top = '0';
-    scrollPoint.style.paddingTop = '25px';
+    scrollPoint.style.paddingTop = '0px';
     headerLogo.style.top = '7px';
     upArrow.style.right = '5px';
   } else {
