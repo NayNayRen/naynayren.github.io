da88a2a4a88a09c006f4fc508bb21cb9b84e7274
Nate M <jeditendencies35@gmail.com>
2022-02-26 17:28:47 -0500

    im gonna loose it...

--- scripts.js
+++ scripts.js
@@ -8,28 +8,28 @@ const headerLogo = document.querySelector('.header-logo');
 const navLogo = document.querySelector('.nav-logo');
 const upArrow = document.querySelector('.up-arrow');
 
-const nextBtn = document.querySelector(".next");
-const prevBtn = document.querySelector(".prev");
-const slide = document.querySelectorAll(".slide");
-let i = 0;
-
-prevBtn.onclick = function () {
-  slide[i].classList.remove("current");
-  i--;
-  if (i < 0) {
-    i = slide.length - 1;
-  }
-  slide[i].classList.add("current");
-};
-
-nextBtn.onclick = function () {
-  slide[i].classList.remove("current");
-  i++;
-  if (i >= slide.length) {
-    i = 0;
-  }
-  slide[i].classList.add("current");
-};
+// const nextBtn = document.querySelector(".next");
+// const prevBtn = document.querySelector(".prev");
+// const slide = document.querySelectorAll(".slide");
+// let i = 0;
+//
+// prevBtn.onclick = function () {
+//   slide[i].classList.remove("current");
+//   i--;
+//   if (i < 0) {
+//     i = slide.length - 1;
+//   }
+//   slide[i].classList.add("current");
+// };
+//
+// nextBtn.onclick = function () {
+//   slide[i].classList.remove("current");
+//   i++;
+//   if (i >= slide.length) {
+//     i = 0;
+//   }
+//   slide[i].classList.add("current");
+// };
 
 // fixed navigation settings for all screens
 function fixedNav() {
@@ -85,12 +85,12 @@ function fixedNav() {
 
 // read more buttons for each project in the projects section
 function readMore(project) {
-  // let dots = document.querySelector(`.project-photo-container[data-project="${project}"] .dots`);
-  // let moreText = document.querySelector(`.project-photo-container[data-project="${project}"] .more`);
-  // let readButton = document.querySelector(`.project-photo-container[data-project="${project}"] .read-button`);
-  let dots = document.querySelector(`.slide[data-project="${project}"] .dots`);
-  let moreText = document.querySelector(`.slide[data-project="${project}"] .more`);
-  let readButton = document.querySelector(`.slide[data-project="${project}"] .read-button`);
+  let dots = document.querySelector(`.project-photo-container[data-project="${project}"] .dots`);
+  let moreText = document.querySelector(`.project-photo-container[data-project="${project}"] .more`);
+  let readButton = document.querySelector(`.project-photo-container[data-project="${project}"] .read-button`);
+  // let dots = document.querySelector(`.slide[data-project="${project}"] .dots`);
+  // let moreText = document.querySelector(`.slide[data-project="${project}"] .more`);
+  // let readButton = document.querySelector(`.slide[data-project="${project}"] .read-button`);
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
