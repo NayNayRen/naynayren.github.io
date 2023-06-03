503a8781677a29d80a483c53708b94c636548306
Nate <jeditendencies35@gmail.com>
2023-06-03 17:48:37 -0400

    2 slides show for desktop, goes to 1 at 1000, slower scroll speed for mobile too

--- scripts/glider.js
+++ scripts/glider.js
@@ -8,43 +8,43 @@ function loadGlider() {
       scrollLock: true,
       draggable: true,
       rewind: true,
+      duration: 2,
       dots: glider.parentNode.querySelector(".dots"),
       arrows: {
         prev: glider.parentNode.querySelector(".glider-prev"),
         next: glider.parentNode.querySelector(".glider-next"),
       },
-      //   responsive: [
-      //     {
-      //       // screens greater than > 400px
-      //       breakpoint: 400,
-      //       settings: {
-      //         slidesToShow: 1,
-      //         slidesToScroll: 1,
-      //         // itemWidth: 150,
-      //         duration: 1,
-      //       },
-      //     },
-      //     {
-      //       // screens greater than > 700px
-      //       breakpoint: 700,
-      //       settings: {
-      //         slidesToShow: 1,
-      //         slidesToScroll: 1,
-      //         // itemWidth: 150,
-      //         duration: 1,
-      //       },
-      //     },
-      //     {
-      //       // screens greater than > 1300px
-      //       breakpoint: 1300,
-      //       settings: {
-      //         slidesToShow: 1,
-      //         slidesToScroll: 1,
-      //         // itemWidth: 150,
-      //         duration: 1,
-      //       },
-      //     },
-      //   ],
+      responsive: [
+        {
+          // screens greater than > 400px
+          breakpoint: 400,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            // itemWidth: 150,
+          },
+        },
+        {
+          // screens greater than > 1000px
+          breakpoint: 1000,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            // itemWidth: 150,
+            duration: 1,
+          },
+        },
+        {
+          // screens greater than > 1300px
+          breakpoint: 1300,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            // itemWidth: 150,
+            duration: 1,
+          },
+        },
+      ],
     });
   });
 }
