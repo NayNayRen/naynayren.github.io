b04fe8ada28cd342ccf17ff8b056cfd9b43afea4
Nate <jeditendencies35@gmail.com>
2023-07-24 16:01:40 -0400

    tweaked some styles and slimmed margin on skills

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -96,7 +96,7 @@ function loadScript() {
         skill.style.marginLeft = "0";
       } else {
         skill.style.opacity = "0";
-        skill.style.marginLeft = "75px";
+        skill.style.marginLeft = "50px";
       }
     });
   }
